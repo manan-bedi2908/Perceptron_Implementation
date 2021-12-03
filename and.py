4607d676569082166316e05192edd26d4b89a1a3
manan-bedi2908 <manan.19535@sscbs.du.ac.in>
2021-12-04 00:42:38 +0530

    modularized code

--- and.py
+++ and.py
@@ -3,22 +3,24 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1]
-}
-df = pd.DataFrame(AND)
-print(df)
+def main(data, eta, epochs, filename, plotFileName):
+    df = pd.DataFrame(data)
+    print(df)
+    X, y = prepare_data(df)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-X, y = prepare_data(df)
-ETA = 0.3
-EPOCHS = 10
+    _ = model.total_loss()
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    save_model(model, filename=filename)
+    save_plot(df, plotFileName, model)
 
-_ = model.total_loss()
-
-save_model(model, filename="and.model")
-save_plot(df, "and.png", model)
+if __name__ == '__main__':
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1]
+    }
+    ETA = 0.3
+    EPOCHS = 10
+    main(data=AND, eta=ETA, epochs=EPOCHS, filename="and.model", plotFileName="and.png")
